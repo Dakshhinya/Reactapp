9eb0e746a336e7830c646fa4f9fcc6e7126d8bd3
Dakshhinya <dakku172004@gmail.com>
2024-09-19 06:45:23 +0530

    projects

--- Portfolio/src/pages/Project.jsx
+++ Portfolio/src/pages/Project.jsx
@@ -3,7 +3,26 @@ import Navbar from "../components/Navbar";
 const Project=()=>{
     return(
         <>
-        <div>Project</div>
+        <div className="w-vh h-vh flex flex-row justify-center items-center p-20">
+<div class="box flex justify-center items-center">
+  <span></span>
+  <div class="content">
+    <h2>Hospital Management</h2>
+  </div>
+</div>
+<div class="box">
+  <span></span>
+  <div class="content">
+    <h2>Facial Recognition based Attendance System</h2>
+  </div>
+</div>
+<div class="box">
+  <span></span>
+  <div class="content">
+    <h2>Portfolio with MERN</h2>
+  </div>
+</div>
+</div>
         </>
     )
 }
